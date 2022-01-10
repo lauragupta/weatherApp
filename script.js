aaf2057bb9aaa1c75d95df920699c6d88e81ed11
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2022-01-09 22:43:07 -0800

    set up html form and link files and jquery and bootstrap and start formatting in the html file plus background to make sure css is linked

---
+++ script.js
@@ -0,0 +1,11 @@
+
+//GIVEN a weather dashboard with form inputs
+var APIKey = "d2d9bc8002577dcf660cef71f30ebecb";
+//search for a city with geolocation api
+//event listener for button
+//city is added to the search history
+//current weather conditions for that city
+//current weather with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+//the UV index color coded to indicate if conditions are favorable, moderate, or severe
+//future  5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+//a city in the search history provides current and future conditions for that city
