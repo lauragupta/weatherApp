a7ae67be97cc0452915989ea03eeee8e2f835d2f
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2022-01-12 00:58:34 -0800

    separate out the functions into smaller functions

--- script.js
+++ script.js
@@ -1,49 +1,29 @@
 var today = moment().format("dddd, MMMM, D, YYYY");
+var APIKey = "d2d9bc8002577dcf660cef71f30ebecb";
+var myWeather = $("#weatherToday");
 var storedCities = getCities();
+
 $.each(storedCities, function(i, city) {
     var $button = $("<button>").text(city);
     $("#cityHistory").append($button);
-})
+});
+
 //GIVEN a weather dashboard with form input
 function searchWeatherInCity(event){
     event.preventDefault();
     // run the search for weather with city name
     var inputCity = $("#weatherLocation").val().trim();
-
-    var APIKey = "d2d9bc8002577dcf660cef71f30ebecb";
-    $.ajax({
-        url: "https://api.openweathermap.org/data/2.5/weather?q=" + inputCity + "&appid=" + APIKey + "&units=imperial",
-        method: "GET"
-    }).then(function(response) {
-        console.log(response);
-
-        myWeather = $("#weatherToday");
-        var currentLat = response.coord.lat;
-        var currentLon = response.coord.lon;
-        //current weather conditions for that city
-        //current weather with the city name, the date, an icon representation of weather conditions, 
-        //the temperature, the humidity, the wind speed
-        var currentCity = $("<h2>").text(response.name);
-        var currentDate = $("<h3>").text(today);
-        var currentIcon = $("<img>").attr("src", "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
-        var currentTemp = $("<h3>").text("Temperature: " + response.main.temp + "°F");
-        var currentHumidity = $("<p>").text("Humidity: " + response.main.humidity + "%");
-        var currentWindSpeed = $("<p>").text("Wind Speed " + response.wind.speed + " MPH");
-        
-
-        myWeather.append(currentCity, currentDate, currentIcon, currentTemp, currentHumidity, currentWindSpeed);
-        getUVAndForcast(currentLat, currentLon, APIKey)
-        var storedCities = getCities();
-        storedCities.unshift(response.name);
-        //create button for input city
-        $.each(storedCities, function(i, city) {
-            var $button = $("<button>").text(city);
-            $("#cityHistory").append($button);
-        })
-     
-        localStorage.setItem("city", JSON.stringify(storedCities));
+    getAndRenderCurrentWeather(inputCity);
+    var storedCities = getCities();
+    storedCities.unshift(inputCity);
+    //city is added to the search history
+    //create button for input city
+    $.each(storedCities, function(i, city) {
+        var $button = $("<button>").text(city);
+        $("#cityHistory").append($button);
+    }) 
+    localStorage.setItem("city", JSON.stringify(storedCities));
     
-    });
 }
 
  //store city information
@@ -56,11 +36,35 @@ function searchWeatherInCity(event){
      return storedCities;
  }
 
+function getAndRenderCurrentWeather(inputCity) {
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/weather?q=" + inputCity + "&appid=" + APIKey + "&units=imperial",
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        var currentLat = response.coord.lat;
+        var currentLon = response.coord.lon;
+        //current weather conditions for that city
+        //current weather with the city name, the date, an icon representation of weather conditions, 
+        //the temperature, the humidity, the wind speed
+        var $currentCity = $("<h2>").text(response.name);
+        var $currentDate = $("<h3>").text(today);
+        var $currentIcon = $("<img>").attr("src", "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
+        var $currentTemp = $("<h3>").text("Temperature: " + response.main.temp + "°F");
+        var $currentHumidity = $("<p>").text("Humidity: " + response.main.humidity + "%");
+        var $currentWindSpeed = $("<p>").text("Wind Speed " + response.wind.speed + " MPH");
+        
+        myWeather.append($currentCity, $currentDate, $currentIcon, $currentTemp, $currentHumidity, $currentWindSpeed);
+        getUVAndForcast(currentLat, currentLon);
+        return inputCity;
+    });
+}
+
 //event listener for button
 $("#weatherSearch").on("click", searchWeatherInCity);
     
 //the UV index color coded to indicate if conditions are favorable, moderate, or severe        
-function getUVAndForcast(currentLat, currentLon, APIKey){
+function getUVAndForcast(currentLat, currentLon) {
     $.ajax({
         url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + currentLat + "&lon=" + currentLon + "&exclude=minutely&appid=" + APIKey + "&units=imperial",
         method: "GET"
@@ -87,7 +91,7 @@ function renderForecast(response) {
         myForecast.append(forecastIcon, forecastTemp, forecastHumidity);
     })
     
-    //var forcastDate = 
+    //var forecastDate = 
 
 }
 
@@ -95,7 +99,6 @@ function renderForecast(response) {
 function colorUVIndex(currentUVIndex, myUVIndex) {
     if (currentUVIndex < 3) {
         $("#uvIndex").css("color", "limegreen");
-        //myUVIndex.append(currentUVIndex);
     } else if (currentUVIndex >= 3 && currentUVIndex < 6) {
         $("#uvIndex").css("color", "yellow");
     } else if (currentUVIndex >= 6 && currentUVIndex < 8) {
@@ -104,24 +107,11 @@ function colorUVIndex(currentUVIndex, myUVIndex) {
         $("#uvIndex").css("color", "red");
     } else if (currentUVIndex >=11) {
         $("#uvIndex").css("color", "purple");
-        //myUVIndex.append(currentUVIndex);
     }
 }
 
-
-
-//search for a city with geolocation api
-//city is added to the search history
 
 //a city in the search history provides current and future conditions for that city
 
 
     //event listener for button
-
-// var APIKey = "d2d9bc8002577dcf660cef71f30ebecb";
-// $.ajax({
-//     url: "http://api.openweathermap.org/geo/1.0/reverse?lat=" + lat + "&lon=" + lon + "&limit=5&appid=" + APIKey;
-//     method: "GET"
-// }).then(function(response) {
-//     console.log(response)
-// });
